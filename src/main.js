4be77772d310871fd8e2abcd7b1267a0b3445b94
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-09-30 16:21:06 +0800

    style

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import '@/styles/index.scss'
 
 const app = createApp(App)
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
+app.use(VueAxios, axios)
